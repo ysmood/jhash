ad8d52e738634321bbe8a27550064fba0eb91aef
Yad Smood <y.s.inside@gmail.com>
2014-04-13 22:43:14 +0800

    add: python implementation.

---
+++ src/ys_hash.py
@@ -0,0 +1,21 @@
+import random
+
+def baseN(num,b,numerals="0123456789abcdefghijklmnopqrstuvwxyz"):
+    return ((num == 0) and numerals[0]) or (baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
+
+def ys_hash(arr):
+	h = reduce(
+		lambda sum, n:
+			((sum << 1 | sum >> 30) & 0x7fffffff) ^ n,
+		arr,
+		8388617
+	)
+	return baseN(h, 36)
+
+# Test
+arr = map(
+	lambda n:
+		random.randint(0, 2 ** 8),
+	range(5000)
+)
+print ys_hash(arr)
