65fdc880d6d82687eb164ac2a23222027112cc34
Yad Smood <y.s.inside@gmail.com>
2014-04-13 22:32:39 +0800

    opt: to_s

--- src/ys_hash.rb
+++ src/ys_hash.rb
@@ -2,9 +2,9 @@
 def ys_hash arr
 	arr.reduce(8388617) { |sum, n|
 		((sum << 1 | sum >> 30) & 0x7fffffff) ^ n
-	}
+	}.to_s(36)
 end
 
 # Test
 arr = (0..5000).map { rand 2 ** 8 }
-puts ys_hash(arr).to_s(36)
+puts ys_hash(arr)
